a2e941854d5f5037d6fd466b8be25f5dbc5825e8
jimbog <jimmygarzon@gmail.com>
2014-03-18 19:25:21 -0400

    add homes controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shouter::Application.config.secret_key_base = '98b21a878fdd1ec0c28157b7f62773f104960ad16fd2012d32de7afc8094d842142fc8ba77161fd8f9d537735ea1f46641d4de9e41dda3100e8852e17da6b159'
