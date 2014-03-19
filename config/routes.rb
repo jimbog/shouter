b8474dbfbd19cfb40640dbaf2ec8f3493e63bd75
jimbog <jimmygarzon@gmail.com>
2014-03-18 20:11:58 -0400

    redirect signed in users to dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Shouter::Application.routes.draw do
   root to: 'homes#show', via: :get
+  resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
